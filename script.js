c7afec17742d00a59d11349cb1d3e50d99348fb1
oreillyalan <oreillyalan132@gmail.com>
2020-08-28 16:42:03 +0100

    added fullscreen functionality

--- script.js
+++ script.js
@@ -111,18 +111,65 @@ function changeSpeed() {
 
 
 // Fullscreen ------------------------------- //
+//  View in fullscreen /
+function openFullscreen(element) {
+  if (element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if (element.mozRequestFullScreen) {
+    /* Firefox */
+    element.mozRequestFullScreen();
+  } else if (element.webkitRequestFullscreen) {
+    /* Chrome, Safari and Opera */
+    element.webkitRequestFullscreen();
+  } else if (element.msRequestFullscreen) {
+    /* IE/Edge */
+    element.msRequestFullscreen();
+  }
+  //add class to element to invoke css
+  video.classList.add('video-fullscreen');
+}
 
+/* Close fullscreen */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    /* Firefox */
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) {
+    /* Chrome, Safari and Opera */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    /* IE/Edge */
+    document.msExitFullscreen();
+  }
+   //remove class from element to invoke css
+  video.classList.remove('video-fullscreen');
+}
 
+let fullscreen = false;
 
+// Toggle fullscreen
+function toggleFullscreen() {
+    if (!fullscreen) {
+      openFullscreen(player);
+    } else {
+      closeFullscreen();
+    }
+    fullscreen = !fullscreen;
+  }
+  
 
 
 // Event Listeners
 playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
 video.addEventListener('ended', showPlayIcon );
-video.addEventListener('timeupdate', updateProgress );
+video.addEventListener('timeupdate', updateProgress);
 video.addEventListener('canplay', updateProgress );
+video.addEventListener('dblclick', toggleFullscreen );
 progressRange.addEventListener('click', setProgress );
 volumeRange.addEventListener('click', changeVolume );
 volumeIcon.addEventListener('click', toggleMute)
 speed.addEventListener('change', changeSpeed);
+fullscreenBtn.addEventListener('click', toggleFullscreen);
